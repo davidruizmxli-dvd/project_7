41731df2a4b8c5844e500248a628a9ab7d057d2b
David Ruiz Miramontes <david.ruizmxli@gmail.com>
2026-01-07 19:14:44 -0800

    actualizacion de codigo proyecto sprint 7

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if st.button("Construir Histograma"):
         fig = px.histogram(df, x='odometer', title='Histograma de odometro')
     else:
         fig = px.histogram(df, x=df.columns[0], title=f'Histograma de {df.columns[0]}')
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, width='stretch')
 
 #boton para grafico de dispersion
 
@@ -20,6 +20,6 @@ if st.button('scatter plot'):
     st.write("Scatter price vs odom")
     if 'odometer'in df.columns and 'price' in df.columns:
         fig2 = px.scatter(df, x='odometer', y='price', title='precio vs odo')
-        st.plotly_chart(fig2, use_container_width=True)
+        st.plotly_chart(fig2, width='stretch')
     else:
         st.write("las columnas no estan disponibles")
